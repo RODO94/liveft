20794075ffeb3c460eea37569b382ad27dba618d
Rory Doak <rory.doak@gmail.com>
2025-04-19 21:33:31 +0100

    delete env file

--- src/pages/Entrance/components/EntranceActions/UserSelect.tsx
+++ src/pages/Entrance/components/EntranceActions/UserSelect.tsx
@@ -2,12 +2,19 @@ import { useNavigate } from "@tanstack/react-router";
 import { userProfiles } from "../../../../data/staticUserData";
 import LargeButton from "../../../../ui/components/LargeButton";
 import { EntranceActionComponent } from "./types";
+import { Box } from "@mui/material";
 
 export const UserSelect: EntranceActionComponent = ({ action }) => {
   const navigate = useNavigate();
   if (action !== "userSelect") return null;
   return (
-    <>
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      gap={4}
+      width={"100%"}
+      maxWidth={"450px"}
+    >
       {userProfiles.map((user) => (
         <LargeButton
           key={user.id}
@@ -18,6 +25,6 @@ export const UserSelect: EntranceActionComponent = ({ action }) => {
           }}
         />
       ))}
-    </>
+    </Box>
   );
 };
