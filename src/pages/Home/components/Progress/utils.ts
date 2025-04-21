b1629e05199ec824e1d9aff201e6dc91db605fdb
Rory Doak <rory.doak@gmail.com>
2025-04-21 14:48:26 +0100

    remove comments and monthsOfYear from Progress utils

--- src/pages/Home/components/Progress/utils.ts
+++ src/pages/Home/components/Progress/utils.ts
@@ -3,31 +3,7 @@ import { LiftRecord } from "../../../../types/lifts";
 import dayjs from "dayjs";
 import { getLiftName } from "../../../../data/staticLiftData";
 
-export const monthsOfYear = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
 export const responsiveBarData = (liftData: LiftRecord[]): BarDatum[] => {
-  /**
-   * We need this to return an array
-   * The array needs to be a collection of objects
-   * Each object will represent a month
-   * Each month in the object will have a key value pair
-   * the key being the lift name
-   * the value being the weight lifted in that month
-   * Each lift should also get a colour attached to it
-   */
   const liftsByMonth = liftData.map((lift) => {
     return {
       month: dayjs(lift.date, "YYYY/MM/DD").format("MMM"),
