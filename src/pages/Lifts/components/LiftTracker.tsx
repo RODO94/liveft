4d0e8a2311d0b408190c267f03316a7d2919679a
Rory Doak <rory.doak@gmail.com>
2025-04-22 14:48:08 +0100

    add graph for lifts

--- src/pages/Lifts/components/LiftTracker.tsx
+++ src/pages/Lifts/components/LiftTracker.tsx
@@ -1,34 +1,37 @@
 import { useParams } from "@tanstack/react-router";
-import { UserLiftTarget } from "../../../types/lifts";
+import { LiftRecord, UserLiftTarget } from "../../../types/lifts";
 import { useEffect, useState } from "react";
 import { roryLiftTargets } from "../../../data/staticLiftTargets";
 import { Box, Button, Typography } from "@mui/material";
 import { theme } from "../../../ui/theme";
+import { ResponsiveBar } from "@nivo/bar";
+import { responsiveBarData } from "../../Home/components/Progress/utils";
+import { liftRecordsTable, lifts } from "../../../data/staticLiftData";
 
 export default function LiftTracker({
   numberOfLifts,
 }: {
   numberOfLifts: number;
 }) {
-  /**
-   * This component should have two boxes
-   * One for the total number of lifts
-   * One for the lift target for the year
-   */
   const { liftId } = useParams({ strict: false });
+  const userId = window.sessionStorage.getItem("user");
 
   const [liftTarget, setLiftTarget] = useState<UserLiftTarget | null>(null);
+  const [liftRecords, setLiftRecords] = useState<LiftRecord[] | null>(null);
 
   useEffect(() => {
-    if (!liftTarget) {
+    if (!liftTarget || !liftRecords) {
       const filteredTargets = roryLiftTargets.filter(
         (target) => target.liftId === liftId
       );
+      const filteredRecords = liftRecordsTable.filter(
+        (lift) => lift.liftId === liftId && lift.userId === userId
+      );
       setLiftTarget(filteredTargets[0]);
+      setLiftRecords(filteredRecords);
     }
-  }, [liftId, liftTarget]);
+  }, [liftId, liftTarget, liftRecords, userId]);
 
-  console.log(liftTarget);
   return (
     <Box p={2}>
       <Box>
@@ -88,6 +91,47 @@ export default function LiftTracker({
             </Typography>
           </Button>
         </Box>
+      </Box>
+      <Box minHeight={"100px"} minWidth={"100px"} height={"40vh"} py={2}>
+        <Typography variant="subtitle1">Lift weight per month</Typography>
+
+        {liftRecords && liftTarget && (
+          <ResponsiveBar
+            data={responsiveBarData(liftRecords)}
+            indexBy={"month"}
+            keys={lifts.map((lift) => lift.slug)}
+            margin={{ top: 10, right: 0, bottom: 50, left: 0 }}
+            padding={0.5}
+            innerPadding={0.5}
+            maxValue={liftTarget.targetWeight + 20}
+            axisLeft={null}
+            markers={[
+              {
+                axis: "y",
+                value: 160,
+                lineStyle: {
+                  stroke: theme.palette.success.light,
+                  strokeWidth: 4,
+                },
+              },
+            ]}
+            colors={{ scheme: "greys" }}
+            axisBottom={{
+              legendPosition: "middle",
+              legendOffset: 32,
+            }}
+            theme={{
+              axis: {
+                ticks: {
+                  text: {
+                    fontSize: "12px",
+                    fill: "white",
+                  },
+                },
+              },
+            }}
+          />
+        )}
       </Box>
     </Box>
   );
