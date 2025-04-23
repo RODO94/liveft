f9c48d22894d57972a44b34dcff0705a33f4ed34
Rory Doak <rory.doak@gmail.com>
2025-04-23 17:52:39 +0100

    setup typescript in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 
 import { configDotenv } from "dotenv";
-import knexConfig from "./knexfile";
+import knexConfig from "./knexfile.js";
+
 const { parsed } = configDotenv();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
