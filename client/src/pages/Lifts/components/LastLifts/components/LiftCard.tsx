10c6c716886f15939088663d475f6ba34146a1d4
Rory Doak <rory.doak@gmail.com>
2025-05-09 15:40:57 +0100

    scaffold new LiftCard modal with inputs and actions

--- client/src/pages/Lifts/components/LastLifts/components/LiftCard.tsx
+++ client/src/pages/Lifts/components/LastLifts/components/LiftCard.tsx
@@ -7,6 +7,12 @@ import CardWrapper from "./CardWrapper";
 import { theme } from "../../../../../ui/theme";
 import CardText from "./CardText";
 import { memo, useState } from "react";
+import LiftModalBase from "../../../../../ui/components/LiftModal/LiftModalBase";
+import LiftModalHeader from "../../../../../ui/components/LiftModal/LiftModalHeader";
+import LiftModalActions from "../../../../../ui/components/LiftModal/LiftModalActions";
+import LiftAutocomplete from "../../../../Home/components/AddLiftModal/LiftInput/LiftAutocomplete";
+import { LiftInformationState } from "../../../../Home/components/AddLiftModal/AddLiftModal";
+import TextInput from "../../../../../ui/components/TextInput";
 
 type BaseLiftCard = {
   isAddButton?: false;
@@ -23,66 +29,119 @@ function LiftCard({
   isAddButton = false,
 }: BaseLiftCard | AddButtonProps) {
   const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const [activeLift, setActiveLift] = useState<LiftInformationState>({
+    id: lift?.liftId || "",
+    name: lift?.liftName || "",
+    slug: lift?.liftSlug || "",
+    weight: lift?.weight || 0,
+    reps: lift?.reps,
+  });
   const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   const handleOpenDialog = () => !openDialog && setOpenDialog(true);
 
+  if (!lift) return;
+
   return (
-    <Box
-      component={"article"}
-      display={"flex"}
-      flexDirection={"column"}
-      gap={"0.25rem"}
-      onClick={handleOpenDialog}
-    >
-      <CardWrapper isAddButton={isAddButton}>
-        <CardText
-          isAddButton={isAddButton}
-          fontWeight={600}
-          text={daysOfWeek[dayjs(lift?.date).day()]}
-        />
-        <CardText
-          isAddButton={isAddButton}
-          fontWeight={400}
-          text={dayjs(lift?.date).format("DD/MM")}
-        />
-      </CardWrapper>
-      <CardWrapper isAddButton={isAddButton}>
-        {isAddButton ? (
-          <Button
-            sx={{
-              padding: "0",
-              color: theme.palette.primary.light,
-              minWidth: "48px",
-            }}
-            onClick={() => {}}
+    <>
+      {openDialog ? (
+        <LiftModalBase
+          open={openDialog}
+          handleClose={() => {
+            setOpenDialog(false);
+          }}
+        >
+          <LiftModalHeader
+            title="Alter your lift record"
+            subtitle="Update or delete the record"
           >
-            <Typography
-              sx={{
-                color: theme.palette.primary.light,
+            <LiftAutocomplete value={activeLift} setValue={setActiveLift} />
+            <TextInput
+              name="weight"
+              value={activeLift.weight}
+              onChange={(e) =>
+                setActiveLift({ ...activeLift, weight: Number(e.target.value) })
+              }
+              label="Weight lifted in 'kg'"
+            />
+            <TextInput
+              name="reps"
+              value={activeLift?.reps || 0}
+              onChange={(e) =>
+                setActiveLift({
+                  ...activeLift,
+                  reps: Number(e.target.value),
+                })
+              }
+              label="Number of reps"
+            />
+            <LiftModalActions
+              actions={{
+                cancel: () => setOpenDialog(false),
+                delete: () => console.log("delete"),
+                update: () => console.log("update"),
               }}
-              textAlign={"center"}
-              variant="body2"
-            >
-              {"+ Add"}
-            </Typography>{" "}
-          </Button>
-        ) : (
-          <Typography
-            color="black"
-            textAlign={"center"}
-            variant="body2"
-          >{`${lift?.weight} kg`}</Typography>
-        )}
-      </CardWrapper>
-      {lift?.reps !== 0 && lift?.reps && (
-        <CardWrapper>
-          <Typography color="black" variant="body2" textAlign={"center"}>
-            {`${lift?.reps} reps`}
-          </Typography>
-        </CardWrapper>
+            />
+          </LiftModalHeader>
+        </LiftModalBase>
+      ) : (
+        <Box
+          component={"article"}
+          display={"flex"}
+          flexDirection={"column"}
+          gap={"0.25rem"}
+          onClick={handleOpenDialog}
+        >
+          <CardWrapper isAddButton={isAddButton}>
+            <CardText
+              isAddButton={isAddButton}
+              fontWeight={600}
+              text={daysOfWeek[dayjs(lift?.date).day()]}
+            />
+            <CardText
+              isAddButton={isAddButton}
+              fontWeight={400}
+              text={dayjs(lift?.date).format("DD/MM")}
+            />
+          </CardWrapper>
+          <CardWrapper isAddButton={isAddButton}>
+            {isAddButton ? (
+              <Button
+                sx={{
+                  padding: "0",
+                  color: theme.palette.primary.light,
+                  minWidth: "48px",
+                }}
+                onClick={() => {}}
+              >
+                <Typography
+                  sx={{
+                    color: theme.palette.primary.light,
+                  }}
+                  textAlign={"center"}
+                  variant="body2"
+                >
+                  {"+ Add"}
+                </Typography>{" "}
+              </Button>
+            ) : (
+              <Typography
+                color="black"
+                textAlign={"center"}
+                variant="body2"
+              >{`${lift?.weight} kg`}</Typography>
+            )}
+          </CardWrapper>
+          {lift?.reps !== 0 && lift?.reps && (
+            <CardWrapper>
+              <Typography color="black" variant="body2" textAlign={"center"}>
+                {`${lift?.reps} reps`}
+              </Typography>
+            </CardWrapper>
+          )}
+        </Box>
       )}
-    </Box>
+    </>
   );
 }
 
