7d04ab5f925ace304ed1716045233d11c7ea34af
Rory Doak <rory.doak@kestrix.io>
2026-01-25 19:16:46 +0000

    fix: make prisma Client files output into generated folder

--- server/database.ts
+++ server/database.ts
@@ -1,23 +1,9 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/index.js";
 import pg from "pg";
 
 const pool = new pg.Pool({ connectionString: process.env.NETLIFY_DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
-
-async function main() {
-  // ... you will write your Prisma Client queries here
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
