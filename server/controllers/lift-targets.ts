0008efee06ff2b9e59b78acf2a0bbf701eb49421
Rory Doak <rory.doak@kestrix.io>
2026-01-25 21:04:26 +0000

    fix imports of prisma client

--- server/controllers/lift-targets.ts
+++ server/controllers/lift-targets.ts
@@ -5,7 +5,7 @@ import { parseUUID } from "./utils.js";
 import { CreateReponse, UpdateReponse } from "../types/utils.js";
 import { UUID } from "crypto";
 import { z } from "zod";
-import { LiftTargets } from "@prisma/client";
+import { LiftTargets } from "../generated/prisma/index.js";
 
 export const getTargetById: RequestHandler = async (req, res) => {
   // Fetch user records from the database
